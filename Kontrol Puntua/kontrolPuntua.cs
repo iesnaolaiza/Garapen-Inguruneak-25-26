78a34b04a016ccb63b2b29c1b38dfe01bb77f862
iesnaolaiza <91121091+iesnaolaiza@users.noreply.github.com>
2026-02-27 08:53:27 +0100

    Kontrol punturako pistak
    Kontrol puntuan sartuko diren jarduerak zein izango diren zehaztu.

--- Kontrol Puntua/kontrolPuntua.cs
+++ Kontrol Puntua/kontrolPuntua.cs
@@ -1,1 +1,6 @@
+//KONTROL PUNTURAKO IKASI BEHARREKOAK
+
+// 1.GhostDoc + Docfx: aplikazio bat iruzkindu eta aplikazio horren dokumentazio teknikoa sortu eta GitHub Page-ra igo.
+// 2.Issueak: GitHub-ren bidez proiektu/programa baten planifikazioa gauzatu, issueak eta milestonak erabilita.
+// 3.Arazte tresnak: kodigoa aztertu breakpoint-ak (baldintzatuak), eta kodigoa zuzendu eta GitHub-era igo.
 
