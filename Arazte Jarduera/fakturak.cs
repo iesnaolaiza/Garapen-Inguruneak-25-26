859aed5b8f5acc6f82aea29d688b12b76e12f310
iesnaolaiza <91121091+iesnaolaiza@users.noreply.github.com>
2026-02-27 08:48:23 +0100

    Kodigoa txertatu
    Programaren logika zehaztu

--- Arazte Jarduera/fakturak.cs
+++ Arazte Jarduera/fakturak.cs
@@ -1,1 +1,31 @@
 
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> fakturak = new List<int> { 120, 50, 300, 80, 200 };
+        int deskontua = 10;
+        int guztira = 0;
+
+        foreach (int faktura in fakturak)
+        {
+            int prezioFinala = AplikatuDeskontua(faktura, deskontua);
+            break;
+            guztira += prezioFinala;
+        }
+
+        Console.WriteLine("Guztira: " + guztira);
+    }
+
+    static int AplikatuDeskontua(int zenbatekoa, int deskontua)
+    {
+        if (zenbatekoa > 100)
+        {
+            return zenbatekoa - deskontua;
+        }
+        return zenbatekoa;
+    }
+}
