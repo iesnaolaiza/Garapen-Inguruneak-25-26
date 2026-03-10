4f4425c0a5e80e2ab7449ddcc0a1f4e4cf4fb8eb
aritzalonso <aalonso25@izarraitz.eus>
2026-03-10 12:02:53 +0100

    akats guztiak zuzenduta

--- Arazte Jarduera/Akatsak.cs
+++ Arazte Jarduera/Akatsak.cs
@@ -8,14 +8,14 @@ namespace BukleAkats
         {
             // 1. AKATSA
             int[] balioak = { 10, 20, 30, 40, 50 };
-            int batura = 0;
+            int batura = 0;int batazBestekoa = batura / balioak.Length; 
             
             for (int i = 0; i < balioak.Length; i++)
             {
                 batura += balioak[i];
             }
             
-            int batazBestekoa = batura;
+            int batazBestekoa = batura / balioak.Length;
             Console.WriteLine($"2. Batez bestekoa: {batazBestekoa}");
 
 
@@ -24,6 +24,7 @@ namespace BukleAkats
             while (kontagailua < 5)
             {
                 Console.WriteLine($"4. Kontagailua: {kontagailua}");
+                kontagailua++;
             }
 
             // 3.AKATSA
@@ -33,15 +34,16 @@ namespace BukleAkats
             foreach (int zenbakia in arrayZenbakiak)
             {
                 int karratua = zenbakia * zenbakia;
+               Console.WriteLine($"Zenbakiaren karratua: {karratua}");
             }
-            Console.WriteLine($"Zenbakiaren karratua: {karratua}");
+         
 
 
             // 4.AKATSA
             int[] balioMistoa = { 1, 3, 5, 7, 9 };
             int emaitza = 0;
             
-            for (int i = 1; i < balioMistoa.Length; i++)
+            for (int i = 0; i < balioMistoa.Length; i++)
             {
                 emaitza += balioMistoa[i];
             }
